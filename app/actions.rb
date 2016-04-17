dab874a3eb5536288160f821e594b7309587c92a
marckuo <marc.kuo@gmail.com>
2016-04-17 19:00:22 +0000

    First commit

---
+++ app/actions.rb
@@ -0,0 +1,32 @@
+require 'pry'
+# Homepage (Root path)
+get '/' do
+  erb :index
+end
+
+get '/messages' do
+  @messages = Message.all
+  erb :'messages/index'
+end
+
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:title],
+    author: params[:author]
+    )
+  if @message.save
+  redirect'/messages'
+  else
+    erb:'messages/new'
+  end
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
